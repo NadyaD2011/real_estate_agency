e79e34a6827f37805726fd895d58138f67479f7e
NadyaEvp <t_evp@mail.ru>
2024-08-22 20:33:15 +0300

    Добавили данные в модель Owner

--- property/admin.py
+++ property/admin.py
@@ -22,6 +22,10 @@ class ComplaintAdmin(admin.ModelAdmin):
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['flats']
+    list_display = ['owner',
+                    'owner_pure_phone',
+                    ]
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
