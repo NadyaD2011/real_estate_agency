b60c48327b45b4314b687f8fb0ef9a432204081f
NadyaEvp <t_evp@mail.ru>
2024-08-24 21:05:49 +0300

    Переименновываем переменные

--- property/models.py
+++ property/models.py
@@ -80,20 +80,14 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField(
-        'ФИО владельца',
-        max_length=200,
-        db_index=True)
-    phonenumber = models.CharField(
-        'Номер владельца',
-        max_length=20)
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        blank=True,
-        null=True)
+    owner = models.CharField("ФИО владельца", max_length=200, db_index=True)
+    phonenumber = models.CharField("Номер владельца", max_length=20)
+    pure_phone = PhoneNumberField(
+        "Нормализованный номер владельца", blank=True, null=True
+    )
     flats = models.ManyToManyField(
         Flat, related_name="owners", verbose_name="Квартиры в собственности"
     )
 
     def __str__(self):
-        return f'{self.owner}, тел. {self.owner_pure_phone}'    
+        return f"{self.owner}, тел. {self.pure_phone}"
