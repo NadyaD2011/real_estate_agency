0de06789682499024273f5f2363fdf75e1b05029
NadyaEvp <t_evp@mail.ru>
2024-08-24 21:07:36 +0300

    переимменновываем перемменною

--- property/admin.py
+++ property/admin.py
@@ -25,7 +25,8 @@ class FlatAdmin(admin.ModelAdmin):
 
 
 class ComplaintAdmin(admin.ModelAdmin):
-    raw_id_fields = ['user', 'complaint_flat']
+    raw_id_fields = ["user", "flat"]
+
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ["flats"]
