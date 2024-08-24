51465e57c3a25dc91faf18f76bd76ab3252d3f7e
NadyaEvp <t_evp@mail.ru>
2024-08-24 21:01:53 +0300

    Настраиваем отступы

--- property/admin.py
+++ property/admin.py
@@ -2,23 +2,25 @@ from django.contrib import admin
 
 from .models import Flat, Complaint, Owner
 
+
 class OwnerInline(admin.TabularInline):
     model = Owner.flats.through
-    raw_id_fields = ['owner']
+    raw_id_fields = ["owner"]
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address']
-    readonly_fields = ['created_at']
-    list_display = ['address',
-                    'price',
-                    'new_building',
-                    'construction_year',
-                    'town',
-                    ]
-    list_editable = ['new_building']
-    list_filter = ('has_balcony', 'new_building', 'rooms_number')
-    raw_id_fields = ['liked_by']
+    search_fields = ["town", "address"]
+    readonly_fields = ["created_at"]
+    list_display = [
+        "address",
+        "price",
+        "new_building",
+        "construction_year",
+        "town",
+    ]
+    list_editable = ["new_building"]
+    list_filter = ("has_balcony", "new_building", "rooms_number")
+    raw_id_fields = ["liked_by"]
     inlines = [OwnerInline]
 
 
